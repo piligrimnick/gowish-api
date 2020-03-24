d74065d498bf679149059e7c887ae9d811fecb36
bogish <nikita.bogomolov@gmail.com>
2020-03-25 01:52:21 +0600

    Setup devise with doorkeeper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Doorkeeper code
+  before_action :doorkeeper_authorize! # Requires access token for all actions
+
+  respond_to :json
+
+  protected
+
+  # Devise methods
+  # Authentication key(:username) and password field will be added automatically by devise.
+  def configure_permitted_parameters
+    added_attrs = [:email]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  private
+
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  use_doorkeeper do
+    skip_controllers :applications, :authorized_applications
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :api, defaults: { format: :json }, path: 'api' do
+    scope module: :v1, constraints: Constraints::ApiConstraint.new(version: 1, default: true) do
+      devise_for :users, controllers: {
+           registrations: 'api/v1/users/registrations',
+       }, skip: [:sessions, :password]
+    end
+  end
 end
