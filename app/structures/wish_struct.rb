2a7a2ac7d3d0d6aca37d36a559d7d0e996a35da6
bogish <nikita.bogomolov@gmail.com>
2020-04-05 02:48:44 +0600

    add created at to wish struct

--- app/structures/wish_struct.rb
+++ app/structures/wish_struct.rb
@@ -2,4 +2,5 @@ class WishStruct < ApplicationStruct
   attribute :body, Types::String.optional
   attribute :url, Types::String.optional
   attribute :user_id, Types::Integer
+  attribute :created_at, Types::Time
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'shwish.app'
+    origins '*'
 
     resource '*',
       headers: :any,
