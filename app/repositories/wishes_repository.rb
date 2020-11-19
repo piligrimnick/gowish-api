ce78449042cbbf37aa44d2828ba1226c0de9d6d6
Nikita Bogomolov <nikita.bogomolov@gmail.com>
2020-11-20 01:56:56 +0600

    Change order

--- app/repositories/wishes_repository.rb
+++ app/repositories/wishes_repository.rb
@@ -2,4 +2,10 @@ class WishesRepository < ApplicationRepository
   def initialize(gateway: Wish, collection: WishesCollection, struct: WishStruct)
     super
   end
+
+  def filter(params)
+    @objects = gateway.order(:created_at).where(params)
+
+    structurize
+  end
 end
