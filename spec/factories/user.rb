bf9682f2971d3217b47466a2deda11a425c75207
Nikita Bogomolov <nbogomolov@taxdome.com>
2025-12-31 01:56:24 +0100

    update specs

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     firstname  { Faker::Name.first_name }
     lastname { Faker::Name.last_name }
     email { Faker::Internet.email }
-    password { Faker:: Internet.password(8) }
+    password { Faker::Internet.password(min_length: 8) }
     password_confirmation { password }
   end
 end
